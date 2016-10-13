f5c2cb56703f4b5a1edc9764fce4df4958d96b2e
Tapesh Nagarwal <tn64@afsconnect2.njit.edu>
2016-10-12 23:46:51 -0400

    Pop Function

---
+++ arrayPop.php
@@ -0,0 +1,8 @@
+<?php
+	
+	$extraElements = array('moving','last','element','extraElement');
+	$enoughElements = array_pop($extraElements);
+	
+	print_r($extraElements);
+	print_r($enoughElements);
+?>
