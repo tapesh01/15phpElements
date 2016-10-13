41daba9eb27825e63903f2614104aa0357eff12a
Tapesh Nagarwal <tn64@afsconnect2.njit.edu>
2016-10-13 02:23:28 -0400

    Sum Function

---
+++ arraySum.php
@@ -0,0 +1,7 @@
+<?php
+
+	$gamesPositionInQue = array('First Game'=>'GTA','Second Game'=>'MW3','Third Game'=>'smashbros');
+	$gameTitles = array_flip($gamePositionInQue);
+	print_r($gamesPositionInQue);
+	
+?>
