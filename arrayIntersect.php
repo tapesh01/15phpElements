4158e6762a0e1dcf065e9befc13b1dd60bac68af
Tapesh Nagarwal <tn64@afsconnect2.njit.edu>
2016-10-13 04:28:02 -0400

    Intersect Function

---
+++ arrayIntersect.php
@@ -0,0 +1,8 @@
+<?php
+	$arr1 = array('Happy', 'Sad', 'Mad', 'Chilling');
+	$arr2 = array('Cool', 'Fat', 'Happy', 'Bored');
+	$repeated = array_intersect($arr1, $arr2);
+	
+	print_r($repeated);
+?>
+
